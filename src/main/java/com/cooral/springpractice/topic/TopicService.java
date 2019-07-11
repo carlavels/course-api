f9e1928e77dc920499a16c46323a9a756afd13b8
Carlavels <--list>
2019-07-11 18:43:34 +0800

    Latest changers - traingin 19 - 28

--- src/main/java/com/cooral/springpractice/topic/TopicService.java
+++ src/main/java/com/cooral/springpractice/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.cooral.springpractice.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,14 +9,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<> (Arrays.asList(
 			new Topic("Spring", "Spring Framework", "Spring Description"),
 			new Topic("Java", "Java Beans", "Java Description"),
 			new Topic("Javascript", "Node JS", "Javascript Description"),
 			new Topic("C", "C Language", "C Description"),
-			new Topic("Python", "Django Framework", "Python-Django Description"),
-			new Topic("Python", "Flask Framework", "Python-Flask Description")
-			);
+			new Topic("Python Django", "Django Framework", "Python-Django Description"),
+			new Topic("Python Flask", "Flask Framework", "Python-Flask Description")
+			));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -25,4 +26,26 @@ public class TopicService {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
 
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {		
+		
+		for(int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				break;
+			}
+		} 
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));	
+	}
+
+	
 }
